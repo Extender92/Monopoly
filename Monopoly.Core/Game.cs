fc427a50b265a599d6aaa43a9a061562b8318a7e
Mohammad Alzuabi <mhamadzu12@hotmail.com>
2023-11-22 15:05:11 +0100

    Add game class

---
+++ Monopoly.Core/Game.cs
@@ -0,0 +1,35 @@
+﻿using Monopoly.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Core
+{
+    internal class Game
+    {
+        private int[] Board { get; } = new int[40];
+        private Dice _dice { get; set; }
+        private List<Player> _players  { get; set;}
+        private List<Street> _streets { get; set; }
+
+        public Game(Dice dice, int numberOfPlayers)
+        {
+            _dice = dice;
+            _streets = new List<Street>();
+            _players = Player.GetNewPlayers(numberOfPlayers);
+        }
+        private void StartGame(int numberOfPlayers)
+        {          
+            while (true)
+            {
+                foreach (var player in _players)
+                {
+
+                }
+            }
+        }
+
+    }
+}
