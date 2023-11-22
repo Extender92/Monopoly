16cde0b828b997cece311799d82c1014375edfea
Mohammad Alzuabi <mhamadzu12@hotmail.com>
2023-11-22 15:08:20 +0100

    Add Console Model class

---
+++ Monopoly.Console/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using Monopoly.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Console.Models
+{
+    public class Card : Street
+    {
+        public string Image { get; set; }
+    }
+}
