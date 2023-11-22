d9aa1332d60b46b70389f971b979056dddb247c2
Mohammad Alzuabi <mhamadzu12@hotmail.com>
2023-11-22 15:02:48 +0100

    Add Models Classes

---
+++ Monopoly.Core/Models/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Core.Models
+{
+    public class Dice
+    {
+        public int DiceOne {  get; set; }
+        public int DiceTwo{  get; set; }
+
+        private static Random _rnd = new Random();
+        public void RoleDice()
+        {
+            DiceOne = _rnd.Next(1,6);
+            DiceTwo = _rnd.Next(1,6);
+        }
+
+        public bool IsDouble()
+        {
+            return DiceOne == DiceTwo;
+        }
+
+    }
+}
--- Monopoly.Core/Models/Player.cs
+++ Monopoly.Core/Models/Player.cs
@@ -8,6 +8,24 @@ namespace Monopoly.Core.Models
 {
     public class Player
     {
-       
+        public string Name { get; set; }
+        public int Money { get; set; }
+        public int Position { get; set; } = 0;
+
+        public Player(string name)
+        {
+            Name = name;
+            Money = 3000;
+        }
+
+        public static List<Player> GetNewPlayers(int numberOfPlayers)
+        {
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                players.Add(new Player("Player " + (i + 1)));
+            }
+            return players;
+        }
     }
 }
