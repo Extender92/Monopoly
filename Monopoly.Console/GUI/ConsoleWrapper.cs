e585e35412f8629ac2b90d1d52a1b7229607f7ed
Extender92 <dann_137@hotmail.com>
2023-11-29 18:33:54 +0100

    Add IConsoleWrapper

---
+++ Monopoly.Console/GUI/ConsoleWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Console.GUI
+{
+    internal class ConsoleWrapper : IConsoleWrapper
+    {
+        public void Clear() => System.Console.Clear();
+
+        public string ReadLine() => System.Console.ReadLine();
+
+        public string ReadKey() => System.Console.ReadKey().KeyChar.ToString();
+
+        public void WriteLine(string s) => System.Console.WriteLine(s);
+
+        public void SetColor(ConsoleColor color) => System.Console.ForegroundColor = color;
+    }
+}
---
+++ Monopoly.Console/GUI/IConsoleWrapper.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Monopoly.Console.GUI
+{
+    internal interface IConsoleWrapper
+    {
+        void Clear();
+        string ReadKey();
+        string ReadLine();
+        void SetColor(ConsoleColor color);
+        void WriteLine(string s);
+    }
+}
