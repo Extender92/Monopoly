31f1226e122eb2e6852b373bef6ff87038110b91
Mohammad Alzuabi <mhamadzu12@hotmail.com>
2023-11-30 15:15:25 +0100

    Add and update test method

--- Monopoly.Tests/ConsoleTests/ConsoleWrapperTests.cs
+++ Monopoly.Tests/ConsoleTests/ConsoleWrapperTests.cs
@@ -11,19 +11,103 @@ namespace Monopoly.Tests.ConsoleTests
     public class ConsoleWrapperTests
     {
         [Fact]
-        public void CanConsoleWriteLineToConsole()
+        public void CanConsoleWriteLinePrintToSystemConsole()
         {
-            //Arrange
-            var mockConsoleWrapper = new Mock<IConsoleWrapper>();
-            var text = "Hello is this test";
+            // Arrange
+            var text = "TestPrint";
+            var mockConsole = new Mock<IConsoleWrapper>();
+            mockConsole.Setup(c => c.WriteLine(It.IsAny<string>()));
 
-            //Act
-            mockConsoleWrapper.Object.WriteLine(text);
+            // Act
+            mockConsole.Object.WriteLine(text);
+
+            // Assert
+            mockConsole.Verify(c => c.WriteLine(text), Times.Once);
+        }
+
+        [Fact]
+        public void ReadLineShouldlCallSystemConsole()
+        {
+            // Arrange
+            var input = "TestInput";
+            var mockConsole = new Mock<IConsoleWrapper>();
+            mockConsole.Setup(c => c.ReadLine()).Returns(input);
+
+            // Arrange
+            var actual = mockConsole.Object.ReadLine();
 
             //Assert
-            mockConsoleWrapper.Verify(x => x.WriteLine(text), Times.Once);
+            Assert.Equal(input, actual);
+            mockConsole.Verify(c => c.ReadLine(), Times.Once);
+
+        }
+
+        [Fact]
+        public void ClearShouldCallSystemConsoleClear()
+        {
+            // Arrange
+            var mockConsole = new Mock<IConsoleWrapper>();
+
+            // Act
+            mockConsole.Object.Clear();
+
+            // Assert
+            mockConsole.Verify(c => c.Clear(), Times.Once);
         }
 
 
+        [Fact]
+        public void ReadKeyShouldCallSystemConsoleReadKey()
+        {
+            // Arrange
+            var mockConsole = new Mock<IConsoleWrapper>();
+            mockConsole.Setup(c => c.ReadKey()).Returns("R");
+
+            // Act
+            var actual = mockConsole.Object.ReadKey();
+
+            // Assert
+            Assert.Equal("R", actual);
+            mockConsole.Verify(c => c.ReadKey(), Times.Once);
+        }
+
+        [Fact]
+        public void SetTextColorShouldCallSystemConsoleSetForegroundColor()
+        {
+            // Arrange
+            var mockConsole = new Mock<IConsoleWrapper>();
+
+            // Act
+            mockConsole.Object.SetTextColor(ConsoleColor.Red);
+
+            // Assert
+            mockConsole.Verify(c => c.SetTextColor(ConsoleColor.Red), Times.Once);
+        }
+
+        [Fact]
+        public void ResetColorShouldCallSystemConsoleResetColor()
+        {
+            // Arrange
+            var mockConsole = new Mock<IConsoleWrapper>();
+
+            // Act
+            mockConsole.Object.ResetColor();
+
+            // Assert
+            mockConsole.Verify(c => c.ResetColor(), Times.Once);
+        }
+
+        [Fact]
+        public void SetPositionShouldCallSystemConsoleSetCursorPosition()
+        {
+            // Arrange
+            var mockConsole = new Mock<IConsoleWrapper>();
+
+            // Act
+            mockConsole.Object.SetPosition(10, 20);
+
+            // Assert
+            mockConsole.Verify(c => c.SetPosition(10, 20), Times.Once);
+        }
     }
 }
