54aebf17cf0948043c943d917fc725fd0c7568d4
Extender92 <dann_137@hotmail.com>
2023-12-09 03:07:15 +0100

    Add new Jail and Transaction classes

---
+++ Monopoly.Core/Jail.cs
@@ -0,0 +1,18 @@
+﻿using Monopoly.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Core
+{
+    internal class Jail
+    {
+        internal void PlayerGoToJail(Player player)
+        {
+            player.Position = 10;
+            player.InJail = true;
+        }
+    }
+}
---
+++ Monopoly.Core/Transaction.cs
@@ -0,0 +1,46 @@
+﻿using Monopoly.Core.Models;
+using Monopoly.Core.Models.Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Core
+{
+    internal class Transaction
+    {
+        internal void PlayerGetSalary(Player player)
+        {
+            player.Money += Game.Rules.Salary;
+        }
+
+        public bool BuyPurchasableSquare(Square square)
+        {
+            //Money -= square.Price;
+            //square.Owner = this;
+            return false;
+        }
+
+        public bool PayRentFromPlayerToPlayer(Player fromPlayer, int rent, Player toPlayer)
+        {
+            if (fromPlayer.Money >= rent)
+            {
+                fromPlayer.Money -= rent;
+                toPlayer.Money += rent;
+                return true;
+            }
+            return false;
+        }
+
+        public void MortgageProperty()
+        {
+
+        }
+
+        public bool RepayMortgageProperty()
+        {
+            return false;
+        }
+    }
+}
