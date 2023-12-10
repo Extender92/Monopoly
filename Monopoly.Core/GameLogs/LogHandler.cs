ad520b5b5c2045ecf1658c58b37638f180216af1
Extender92 <dann_137@hotmail.com>
2023-12-10 05:50:15 +0100

    Ádd GameLogs

---
+++ Monopoly.Core/GameLogs/LogHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Core.Logs
+{
+    internal class LogHandler
+    {
+        public List<Log> Logs { get; set; }
+
+        public LogHandler()
+        {
+            List<Log> logs = new List<Log>();
+        }
+
+        internal void CreateLog(string text)
+        {
+            Log log = new Log();
+            log.Id = Logs.Count;
+            log.Info = text;
+            Logs.Add(log);
+        }
+    }
+}
