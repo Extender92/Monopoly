52da618f4793f1906e501a08cbf401758a0528eb
Extender92 <dann_137@hotmail.com>
2023-12-10 05:47:38 +0100

    Add Event Listener

---
+++ Monopoly.Core/Events/PlayerEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Monopoly.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Core.Events
+{
+    internal class PlayerEventArgs : EventArgs
+    {
+        public Player Player { get; }
+        public int TargetSum { get; }
+
+        public PlayerEventArgs(Player player, int targetSum = 0)
+        {
+            Player = player;
+            TargetSum = targetSum;
+        }
+    }
+}
---
+++ Monopoly.Core/Events/SquareEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Monopoly.Core.Models.Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Core.Events
+{
+    internal class SquareEventArgs : EventArgs
+    {
+        public Square Square { get; }
+
+        public SquareEventArgs(Square square)
+        {
+            Square = square;
+        }
+
+        public delegate bool SquareEventHandler(object sender, SquareEventArgs e);
+    }
+}
