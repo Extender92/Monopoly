274cd1f4374f463e69d7dac6f712d930a1eafdd2
Extender92 <dann_137@hotmail.com>
2023-12-11 02:38:53 +0100

    Change Jail Function

--- Monopoly.Core/Jail.cs
+++ Monopoly.Core/Jail.cs
@@ -1,6 +1,9 @@
-﻿using Monopoly.Core.Models;
+﻿using Monopoly.Core.Events;
+using Monopoly.Core.Models;
+using Monopoly.Core.Models.Board;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +12,89 @@ namespace Monopoly.Core
 {
     internal class Jail
     {
+        private Dictionary<Player, PlayerJailInfo> playersInJail = new Dictionary<Player, PlayerJailInfo>();
+
+        public int MaxTurnsInJail { get; } = 3;
+        public int Fine { get; } = 50;
+        private const int JailPosition = 10;
+
         internal void PlayerGoToJail(Player player)
         {
-            player.Position = 10;
+            player.Position = JailPosition;
             player.InJail = true;
+            playersInJail[player] = new PlayerJailInfo();
+            Game.Logs.CreateLog($"{player.Name} went to jail");
+        }
+
+        internal void TakeTurnInJail(Player player)
+        {
+            if (playersInJail.TryGetValue(player, out PlayerJailInfo jailInfo))
+            {
+                bool playerWantsToBuyOut = false;
+
+                if (player.NumberOfGetOutOFJailCards > 0 || Game.CanAffordWithAssets(player, Fine))
+                    playerWantsToBuyOut = GameEvents.InvokeAskPlayerToBuyOutOfJail(player, new PlayerEventArgs(player));
+
+                if (playerWantsToBuyOut)
+                {
+                    BuyOutPlayerFromJail(player);
+                    return;
+                }
+
+                if (RollDiceInJailAndCheckForDouble(player))
+                {
+                    ReleasePlayerFromJail(player);
+                    return;
+                }
+
+                jailInfo.TurnsInJail++;
+
+                if (!(jailInfo.TurnsInJail < MaxTurnsInJail))
+                {
+                    if (Game.IsPlayerBankrupt(player, Fine))
+                    {
+                        Game.HandlePlayerBankruptcy(player);
+                        return;
+                    }
+                    ReleasePlayerFromJail(player);
+                }
+            }
+            else
+            {
+                throw new Exception($"{player.Name} is not in jail!");
+            }
+        }
+
+        private void BuyOutPlayerFromJail(Player player)
+        {
+            while (!Game.Transaction.PayFines(player, Fine))
+            {
+                GameEvents.InvokePlayerInsufficientFunds(player, Fine);
+            }
+            Game.RollDice();
+            ReleasePlayerFromJail(player);
+        }
+
+        private bool RollDiceInJailAndCheckForDouble(Player player)
+        {
+            Game.RollDice();
+            return Game.IsDiceDouble();
+        }
+
+        private void ReleasePlayerFromJail(Player player)
+        {
+            // Remove the player from the playersInJail dictionary
+            playersInJail.Remove(player);
+
+            player.InJail = false;
+            Game.Logs.CreateLog($"{player.Name} was released from jail");
+
+            player.Position += Game.CalculateDiceSum();
+        }
+
+        private class PlayerJailInfo
+        {
+            public int TurnsInJail { get; set; }
         }
     }
 }
